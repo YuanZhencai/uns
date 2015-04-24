c9f3e381892b58fd595df1e4f9facf0b0ba762c0
Yuan Zhencai <zhencai.yuan@sunlights.cc>
2015-04-24 11:20:12 +0800

    config

--- src/main/scala/com/wcs/uns/UnsApp.scala
+++ src/main/scala/com/wcs/uns/UnsApp.scala
@@ -1,30 +1,32 @@
 package com.wcs.uns
 
-import akka.kernel.Bootable
-
-import spray.can.Http
-import com.typesafe.config.ConfigFactory
-import akka.actor.{ActorSystem, Props}
-import com.wcs.uns.service._
+import akka.actor.{Props, ActorSystem}
 import akka.camel.CamelExtension
-
 import akka.io.IO
-
-import org.apache.activemq.camel.component.ActiveMQComponent
+import com.typesafe.config.ConfigFactory
 import com.wcs.uns.api.RestActor
 import com.wcs.uns.model.{UserMessage, Notification, SendNotification}
+import com.wcs.uns.service.{JmsAdapter, Notifier}
+import org.apache.activemq.camel.component.ActiveMQComponent
+import spray.can.Http
 
-class Bootstrapper extends Bootable {
-
+/**
+ * Created by Yuan on 2015/4/24.
+ */
+object UnsApp {
   val config = ConfigFactory.load()
   implicit val system = ActorSystem(s"uns2-${config.getString("env.tag")}")
+
+  def main(args: Array[String]) {
+    startup
+  }
 
   def startup = {
     val camel = CamelExtension(system)
     camel.context.addComponent("activemq", ActiveMQComponent.activeMQComponent(config.getString("activemq.url")))
     val notifier = system.actorOf(Props[Notifier], "notifier")
     val jmsAdapter = system.actorOf(Props[JmsAdapter], "jms-adapter")
-    notifier ! SendNotification(Notification(java.util.UUID.randomUUID().toString, "cmdpms", System.currentTimeMillis().toString, "0", "", UserMessage("cmdpms", "QWEASD123", "1", "zhencai.yuan@sunlights.me", "18321718279", "", s"UNS2邮件发送 - ${config.getString("env.tag")}", s"消息来自: ${java.net.InetAddress.getLocalHost} - ${config.getString("env.tag")}", "")))
+    notifier ! SendNotification(Notification(java.util.UUID.randomUUID().toString, "cmdpms", System.currentTimeMillis().toString, "0", "", UserMessage("cmdpms", "QWEASD123", "1", "zhencai.yuan@sunlights.cc", "18321718279", "", s"UNS2邮件发送 - ${config.getString("env.tag")}", s"消息来自: ${java.net.InetAddress.getLocalHost} - ${config.getString("env.tag")}", "")))
     val server = system.actorOf(Props[RestActor], "server")
     IO(Http) ! Http.Bind(server, config.getString("rest.listening"), config.getInt("rest.port"))
   }
